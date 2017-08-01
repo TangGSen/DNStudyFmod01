d756c535e2cf6b674dadbf944b0293988807d6ae
Administrator <Jasonsens17@gamil.com>
2017-08-02 00:10:54 +0800

    捕获异常

--- fmod/src/main/cpp/effect.cpp
+++ fmod/src/main/cpp/effect.cpp
@@ -26,40 +26,48 @@ JNIEXPORT void JNICALL Java_sen_com_fmod_EffectUtils_playNormal
     bool   playing = false;
     //创建声音
    // const char *name_or_data, FMOD_MODE mode, FMOD_CREATESOUNDEXINFO *exinfo, Sound **sound)
+    try {
 
-    system->createSound(cFilePath,FMOD_DEFAULT,0,&sound);
+        system->createSound(cFilePath,FMOD_DEFAULT,0,&sound);
 
-    switch (jType){
-        case TYPE_NORMAL:
-            LOG_E("TYPE_NORMAL %d",jType);
-            system->playSound(sound,NULL, false,&channel);
-            system->update();
+        switch (jType){
+            case TYPE_NORMAL:
+                LOG_E("TYPE_NORMAL %d",jType);
+                system->playSound(sound,NULL, false,&channel);
+                system->update();
 
 
-            break;
-        case TYPE_LUOLI:
-            system->playSound(sound, NULL, false, &channel);
-            system->update();
-            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&dsp);
-            //设置音调参数
-            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,1.8);
-            channel->addDSP(0,dsp);
-            break;
-        case TYPE_DASHU:
-            system->playSound(sound, NULL, false, &channel);
-            system->update();
-            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
-            //设置音调参数
-            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.8);
-            channel->addDSP(0, dsp);
-            break;
+                break;
+            case TYPE_LUOLI:
+                system->playSound(sound, NULL, false, &channel);
+                system->update();
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&dsp);
+                //设置音调参数
+                dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,1.8);
+                channel->addDSP(0,dsp);
+                break;
+            case TYPE_DASHU:
+                system->playSound(sound, NULL, false, &channel);
+                system->update();
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
+                //设置音调参数
+                dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.8);
+                channel->addDSP(0, dsp);
+                break;
+        }
+    }catch (...){
+        //...代表捕获很多异常
+        goto end;
     }
+
     channel->isPlaying(&playing);
     //每秒判断一下
     while (playing){
         channel->isPlaying(&playing);
         usleep(1000*1000);
     }
+    goto end;
+    end:
     sound->release();
     system->close();
     system->release();
